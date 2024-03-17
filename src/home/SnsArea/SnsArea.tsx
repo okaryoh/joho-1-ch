b574fbd01eb7f39bc4f4af3e831c46ef8613921a
okaryoh <okatyan1997@gmail.com>
2024-03-18 07:57:43 +0900

    xのスクリプトの埋め込みを修正

--- src/home/SnsArea/SnsArea.tsx
+++ src/home/SnsArea/SnsArea.tsx
@@ -5,14 +5,22 @@ export interface SnsAreaProps {
 }
 
 export const SnsArea = ({ }: SnsAreaProps) => {
-  // ココナラのスクリプトを埋め込む https://begien.com/article/58/view
+  // ココナラとXのスクリプトを埋め込む https://begien.com/article/58/view
   const coconalaScript = document.createElement('script');
   coconalaScript.innerHTML = `!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://coconala.com/js/coconala_widget.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'coconala-wjs');`;
+  const xScript = document.createElement('script');
+  xScript.setAttribute('async', '');
+  xScript.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+  xScript.setAttribute('charSet', 'utf-8');
 
   useEffect(() => {
     if (coconalaScript !== null) {
       const coconalaEle = document.getElementById("coconala");
       coconalaEle?.appendChild(coconalaScript);
+    }
+    if (xScript !== null) {
+      const xEle = document.getElementById("x");
+      xEle?.appendChild(xScript);
     }
   })
   // ココナラのスクリプトを埋め込む
@@ -28,10 +36,12 @@ export const SnsArea = ({ }: SnsAreaProps) => {
       overflow: 'scroll'
     }}>
       <Paragraph text="YouTubeで解説動画上げてます！" backgroundColor='#f0f0f0' />
-      <Paragraph text="X(旧Twitter)やってます！" backgroundColor='#f0f0f0' />
-      <div>
-        <a className="twitter-timeline" data-lang="ja" data-width="220" data-height="400" href="https://twitter.com/tohoku_joho_1ch?ref_src=twsrc%5Etfw">Tweets by tohoku_joho_1ch</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
       <iframe src="https://www.youtube.com/embed/B4oW_ZhhrqI?si=6drIB7t_wuRYddxL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <div id="x">
+        <Paragraph text="X(旧Twitter)やってます！" backgroundColor='#f0f0f0' />
+        <div>
+          <a className="twitter-timeline" data-lang="ja" data-height="400" href="https://twitter.com/tohoku_joho_1ch?ref_src=twsrc%5Etfw">Tweets by tohoku_joho_1ch</a>
+        </div>
       </div>
       <div id="coconala">
         <Paragraph text="ココナラでマンツーマン指導やってます！" backgroundColor='#f0f0f0' />
