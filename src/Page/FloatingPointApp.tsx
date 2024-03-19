2fbd93be17422dc95abebcf8a28ff1f6dc62533e
okaryoh <okatyan1997@gmail.com>
2024-03-20 01:48:35 +0900

    FloatingPointApp という、浮動小数点計算のコンポーネントを追加

---
+++ src/Page/FloatingPointApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../style.css';
+import { Home } from './../home/Home';
+
+
+function FloatingPointApp() {
+  return (
+    <div>こんにちは！</div>
+  );
+}
+
+export default FloatingPointApp;
